fe509efa1a0e2abf95e24722f4ee5877a92f8888
Miquel Mariño Espinosa <miquel@MacBook-Air-de-Miquel.local>
2020-08-22 16:30:01 +0200

    forbidden problem to be fixed

--- src/Command/RealTimePublisher.php
+++ src/Command/RealTimePublisher.php
@@ -5,9 +5,17 @@ namespace RealTime\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Mercure\PublisherInterface;
+use Symfony\Component\Mercure\Update;
 
 class RealTimePublisher extends Command
 {
+  private PublisherInterface $publisher;
+  public function __construct(PublisherInterface $publisher) {
+    $this->publisher = $publisher;
+    parent::__construct();
+  }
+
   protected static $defaultName = 'real-time:publish';
   protected function configure()
   {
@@ -17,6 +25,11 @@ class RealTimePublisher extends Command
   
   protected function execute(InputInterface $input, OutputInterface $output)
   {
+    $update = new Update(
+      'http://example.com/books/1',
+      json_encode(['status' => 'OutOfStock'])
+    );
+    ($this->publisher)($update);
     $output->writeln('<info>Success!!</info>');
     return Command::SUCCESS;
   }
